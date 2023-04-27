e89e2ba9370609b193346631c20c3b7fc0c0a9b1
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-04-27 07:18:07 +0600

    up to private route

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import './Login.css'
+import { Link } from "react-router-dom";
+import { AuthContext } from '../Providers/AuthProvider';
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    // const [error, setError] = useState('')
+    const navigate = useNavigate();
+
+    const {signIn} = useContext(AuthContext);
+
+    const handleSignIn=(event)=>{
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password)
+
+        signIn(email, password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser)
+            form.reset()
+            navigate('/')
+        })
+        .catch(error=>{
+            console.log(error)
+           
+        })
+
+        if(password<6){
+            setError('Please provide atleast 6 characters')
+        }
+    }
+
+
+
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleSignIn}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            {/* <p>{error}</p>/ */}
+            <p><small>New to Ema-jogn? <Link to="/signup">Create new account</Link> </small> </p>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4qhZO_UZ_2b1JqF98DjSyd-UhlW10GSw",
+  authDomain: "ema-john-firebase-auth-e1030.firebaseapp.com",
+  projectId: "ema-john-firebase-auth-e1030",
+  storageBucket: "ema-john-firebase-auth-e1030.appspot.com",
+  messagingSenderId: "429372495517",
+  appId: "1:429372495517:web:0ab654507fd3ae8520e674"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
